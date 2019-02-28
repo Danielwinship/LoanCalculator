b023030ff3fca51bda11584d0e20c902e340282a
Daniel Winship <danielwinship@icloud.com>
2019-02-28 05:52:39 -0500

    completed building out calculations in func

--- app.js
+++ app.js
@@ -13,7 +13,22 @@ function calculateResults(e) {
   const totalPayment = document.getElementById('total-payment');
   const totalInterest = document.getElementById('total-interest');
 
-  
+const principal = parseFloat(amount.value);
+const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+const calculatedPayments = parseFloat(year.value) * 12;
+
+
+// Monthly Payments
+const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+const monthly = (principal * x * calculatedInterest)/(x -1);
+
+if (isFinite(monthly)) {
+  monthlyPayment.value = monthly.toFixed(2);
+  totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+  totalInterest.value = ((monthly * calculatedPayments) -principal).toFixed(2);
+} else {
+  console.log('missing data');
+}
 
 
   e.preventDefault();
