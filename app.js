ceb0ac3e5641a28bae0263bb54bfe66d19cdb48e
Daniel Winship <danielwinship@icloud.com>
2019-02-28 06:07:55 -0500

    added gif UX element

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+// Hide results
+document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
   
   // UI Vars
   const amount = document.getElementById('amount');
@@ -26,16 +36,28 @@ if (isFinite(monthly)) {
   monthlyPayment.value = monthly.toFixed(2);
   totalPayment.value = (monthly * calculatedPayments).toFixed(2);
   totalInterest.value = ((monthly * calculatedPayments) -principal).toFixed(2);
+
+  // Show Results
+  document.getElementById('results').style.display = 'block';
+
+  // Hide Loader
+  document.getElementById('loading').style.display = 'none';
+
 } else {
   showError('Please check your numbers');
 }
 
-
-  e.preventDefault();
 }
 
 // Show Error
 function showError(error) {
+
+    // Show Results
+    document.getElementById('results').style.display = 'none';
+
+    // Hide Loader
+    document.getElementById('loading').style.display = 'none';
+    
   // Create Div
   const errorDiv = document.createElement('div');
 
